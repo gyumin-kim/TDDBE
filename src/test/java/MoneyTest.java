83bdaa4511bb4101893efa7cdc6efa2c4ec6a733
Gyumin Kim <starkying@gmail.com>
2019-12-29 00:43:23 +0900

    5장 예제코드 구현 (5CHF x 2 = 10CHF)

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -5,15 +5,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MoneyTest {
 
 	@Test
-	public void testMultiplication() {
+	void testMultiplication() {
 		Dollar five = new Dollar(5);
 		assertThat(five.times(2)).isEqualTo(new Dollar(10));
 		assertThat(five.times(3)).isEqualTo(new Dollar(15));
 	}
 
 	@Test
-	public void testEquality() {
+	void testEquality() {
 		assertThat(new Dollar(5)).isEqualTo(new Dollar(5));
 		assertThat(new Dollar(5)).isNotEqualTo(new Dollar(6));
+	}
+
+	@Test
+	void testFrancMultiplication() {
+		Franc five = new Franc(5);
+		assertThat(five.times(2)).isEqualTo(new Franc(10));
+		assertThat(five.times(3)).isEqualTo(new Franc(15));
 	}
 }
